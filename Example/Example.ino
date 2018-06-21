ec9fcc05e2cdd252931a76746adf72627a4c8d1b
kuroppe <kuroppems@gmail.com>
2018-06-21 20:48:36 +0900

    Will be able to call a interrupt every 5 second to send to xbee

--- Example/Example.ino
+++ Example/Example.ino
@@ -2,24 +2,32 @@
 #include <MsTimer2.h>
 #include "SchmittTrigger.h"
 
-#define CURRENT_SENSOR 0
-#define THRESHOLD_VOLTAGE 4.0
+#define CURRENT_SENSOR 0            //電流センサのpin番号
+#define INTERRUPT_TIME 100          //割り込み間隔[ms]
+#define SEND_TIME 5000              //xbeeの送信間隔[ms]                 
+#define THRESHOLD_VOLTAGE 4.0       //しきい値電圧[V]
 
 float current_sum = 0;              //電流センサの値の合計
 unsigned int read_count = 0;        //センサを読み込んだ回数をカウント
-volatile boolean interrupt = false; //割り込み許可フラグ
 
+typedef volatile struct {
+    boolean flag = false;           //割り込み許可フラグ
+    unsigned int count = 0;         //割り込み回数
+} Interrupt;
+
+Interrupt interrupt;
 SchmittTrigger schmittTrigger = SchmittTrigger(THRESHOLD_VOLTAGE); //しきい値電圧を設定
 
 void interrupt_switch() {
-    interrupt = true;
+    interrupt.flag = true;
+    interrupt.count++;
 }
 
 void setup() {
     Serial.begin(9600);
     PORTC = 0x00;   //A0~A5を入力pinに設定する
     ADCSRA &= 0xFC; //分周比を16に設定する
-    MsTimer2::set(100, interrupt_switch);
+    MsTimer2::set(INTERRUPT_TIME, interrupt_switch);
     MsTimer2::start();
 }
 
@@ -32,15 +40,15 @@ void loop() {
     current_sum += analogRead(CURRENT_SENSOR);
     read_count++;
 
-    if (interrupt == true) {
+    if (interrupt.flag == true) {
         power_on = schmittTrigger.isHigh(calc_average());
-        Serial.println(power_on);
         current_sum = 0;
         read_count = 0;
-        interrupt = false;
+        interrupt.flag = false;
     }
 
-    if (power_on == true) {
-        //溶接機の電源がONのときに実行する処理
+    if (interrupt.count * INTERRUPT_TIME >= SEND_TIME) {
+        //親機へ送信するための処理
+        interrupt.count = 0;
     }
 }
