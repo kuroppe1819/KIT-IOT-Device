a4cb3a84a7b829a640925d730b0eba9dd46020c4
kuroppe <kuroppems@gmail.com>
2018-06-22 15:25:29 +0900

    Enabled a input of multiple sensors

--- Example/Example.ino
+++ Example/Example.ino
@@ -2,25 +2,31 @@
 #include <MsTimer2.h>
 #include "SchmittTrigger.h"
 
-#define CURRENT_SENSOR 0            //電流センサのpin番号
-#define INTERRUPT_TIME 100          //割り込み間隔[ms]
-#define SEND_TIME 5000              //xbeeの送信間隔[ms]                 
-#define THRESHOLD_VOLTAGE 4.0       //しきい値電圧[V]
+#define CURRENT_SENSOR 1                //電流センサの数
+#define INTERRUPT_TIME 100              //割り込み間隔[ms]
+#define SEND_TIME 5000                  //xbeeの送信間隔[ms]                 
+#define THRESHOLD_VOLTAGE 3.0           //しきい値電圧[V]
 
-float current_sum = 0;              //電流センサの値の合計
-unsigned int read_count = 0;        //センサを読み込んだ回数をカウント
+float current_sum[CURRENT_SENSOR] = {}; //電流センサの値の合計
+unsigned int read_count = 0;            //センサを読み込んだ回数をカウント
 
 typedef volatile struct {
-    boolean enable = false;           //割り込み許可フラグ
-    unsigned int count = 0;         //割り込み回数
+    boolean enable = false;             //割り込み許可フラグ
+    unsigned int count = 0;             //割り込み回数
 } Interrupt;
 
 Interrupt interrupt;
-SchmittTrigger schmittTrigger = SchmittTrigger(THRESHOLD_VOLTAGE); //しきい値電圧を設定
 
 void interrupt_switch() {
     interrupt.enable = true;
     interrupt.count++;
+}
+
+void clear_variable() {
+    for (int i =0; i < CURRENT_SENSOR; i++) {
+        current_sum[i] = 0;
+    }
+    read_count = 0;
 }
 
 void setup() {
@@ -31,25 +37,28 @@ void setup() {
     MsTimer2::start();
 }
 
-float calc_average() {
-    return current_sum / read_count * 5 / 1024;
+float calc_average(float sum, unsigned int count) {
+    return sum / count * 5 / 1024;
 }
 
 void loop() {
-    boolean power_on = false; //溶接機の電源
-    current_sum += analogRead(CURRENT_SENSOR);
+    boolean power_on[CURRENT_SENSOR] = {}; //溶接機の電源
+
+    for (int i = 0; i < CURRENT_SENSOR; i++) {
+        current_sum[i] += analogRead(i);
+    }
     read_count++;
 
     if (interrupt.enable) {
-        power_on = schmittTrigger.isHigh(calc_average());
-        current_sum = 0;
-        read_count = 0;
+        for (int i = 0; i < CURRENT_SENSOR; i++) {
+            power_on[i] = calc_average(current_sum[i], read_count) > THRESHOLD_VOLTAGE;
+        }
+        clear_variable();
         interrupt.enable = false;
     }
 
     if (interrupt.count * INTERRUPT_TIME >= SEND_TIME) {
         //親機へ送信するための処理
-        Serial.println(power_on);
         interrupt.count = 0;
     }
 }
