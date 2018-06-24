3c8d2667e88aaf57c758c74e165f6c9d0f68cc2f
kuroppe <kuroppems@gmail.com>
2018-06-24 14:38:10 +0900

    Created a sample of RTC(DS1307)

---
+++ RTC/RTC.ino
@@ -0,0 +1,30 @@
+#include <Arduino.h>
+#include <RTClib.h>
+
+RTC_DS1307 rtc;
+
+void setup() {
+    Serial.begin(9600);
+    if (!rtc.begin()) {
+        Serial.println("Couldn't find RTC");
+        while (1);
+    }
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); //コンパイルした時の時間をRTCに記録する
+}
+
+void loop() {
+    DateTime now = rtc.now();
+    Serial.print(now.year());
+    Serial.print('/');
+    Serial.print(now.month());
+    Serial.print('/');
+    Serial.print(now.day());
+    Serial.print(' ');
+    Serial.print(now.hour());
+    Serial.print(':');
+    Serial.print(now.minute());
+    Serial.print(':');
+    Serial.print(now.second());
+    Serial.println();
+    delay(1000);
+}
