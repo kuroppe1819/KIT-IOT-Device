5db14528e7183263ff63d8d681d413def0fb51f3
kuroppe <kuroppems@gmail.com>
2018-06-30 14:34:16 +0900

    Set format style to WebKit

--- Example/Example.ino
+++ Example/Example.ino
@@ -1,34 +1,38 @@
 #include <Arduino.h>
 #include <Metro.h>
 
-#define CURRENT_SENSOR 1                //電流センサの数
-#define INTERRUPT_TIME 500              //割り込み間隔[ms]
-#define SEND_TIME 5000                  //xbeeの送信間隔[ms]                 
-#define THRESHOLD_VOLTAGE 0.05           //しきい値電圧[V]
+#define CURRENT_SENSOR 1 //電流センサの数
+#define INTERRUPT_TIME 500 //割り込み間隔[ms]
+#define SEND_TIME 5000 //xbeeの送信間隔[ms]
+#define THRESHOLD_VOLTAGE 0.05 //しきい値電圧[V]
 
 float current_sum[CURRENT_SENSOR] = {}; //電流センサの値の合計
-unsigned int read_count = 0;            //センサを読み込んだ回数をカウント
+unsigned int read_count = 0; //センサを読み込んだ回数をカウント
 Metro average_metro = Metro(INTERRUPT_TIME);
 Metro send_metro = Metro(SEND_TIME);
 
-void clear_variable() {
-    for (int i =0; i < CURRENT_SENSOR; i++) {
+void clear_variable()
+{
+    for (int i = 0; i < CURRENT_SENSOR; i++) {
         current_sum[i] = 0;
     }
     read_count = 0;
 }
 
-void setup() {
+void setup()
+{
     Serial.begin(9600);
-    PORTC = 0x00;   //A0~A5を入力pinに設定する
+    PORTC = 0x00; //A0~A5を入力pinに設定する
     ADCSRA &= 0xFC; //分周比を16に設定する
 }
 
-float calc_average(float sum, unsigned int count) {
+float calc_average(float sum, unsigned int count)
+{
     return sum / count * 5 / 1024;
 }
 
-boolean is_power_on(boolean *power_on) {
+boolean is_power_on(boolean* power_on)
+{
     for (int i = 0; i < CURRENT_SENSOR; i++) {
         if (power_on[i] == true) {
             return true;
@@ -37,7 +41,8 @@ boolean is_power_on(boolean *power_on) {
     return false;
 }
 
-void loop() {
+void loop()
+{
     boolean power_on[CURRENT_SENSOR] = {}; //溶接機の電源
 
     for (int i = 0; i < CURRENT_SENSOR; i++) {
