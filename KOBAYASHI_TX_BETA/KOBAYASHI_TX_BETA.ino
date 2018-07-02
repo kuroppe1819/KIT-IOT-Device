60766f4fe322aaf95cfe0d5ad62b996ad93bafcb
kuroppe <kuroppems@gmail.com>
2018-07-02 14:05:36 +0900

    Put a date time and value of sensor in payload that is sent with xbee

--- KOBAYASHI_TX_BETA/KOBAYASHI_TX_BETA.ino
+++ KOBAYASHI_TX_BETA/KOBAYASHI_TX_BETA.ino
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <Metro.h>
+#include <RTClib.h>
 #include <XBee.h>
 
 #define CURRENT_SENSOR_PORT 0 //電流センサが繋がっているポート番号
@@ -15,10 +16,26 @@ Metro average_metro = Metro(INTERRUPT_TIME);
 XBee xbee = XBee();
 XBeeAddress64 addr64 = XBeeAddress64(0x0013a200, 0x4166e492);
 
+RTC_DS1307 rtc;
+
 void setup()
 {
+    /** Serial Communication **/
     Serial.begin(9600);
-    // xbee.setSerial(Serial);
+    xbee.setSerial(Serial);
+
+    /** RTC **/
+    if (!rtc.begin()) {
+        Serial.println("Couldn't find RTC");
+        while (1)
+            ;
+    }
+    if (!rtc.isrunning()) {
+        Serial.println("RTC is NOT running!");
+        rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); //コンパイルした時の時間をRTCに記録する
+    }
+
+    /** PORT **/
     PORTC = 0x00; //A0~A5を入力pinに設定する
     ADCSRA &= 0xFC; //分周比を16に設定する
     delay(10);
@@ -27,6 +44,26 @@ void setup()
 float calc_average(float sum, unsigned int count)
 {
     return sum / count * 5 / 1024;
+}
+
+void pack_data_in_array(uint8_t* send_data, uint8_t payload)
+{
+    DateTime now = rtc.now();
+    send_data[0] = now.year() % 100;
+    send_data[1] = now.month();
+    send_data[2] = now.day();
+    send_data[3] = now.hour();
+    send_data[4] = now.minute();
+    send_data[5] = now.second();
+    send_data[6] = payload;
+}
+
+void send_to_xbee(uint8_t payload)
+{
+    uint8_t send_data[7] = {};
+    pack_data_in_array(send_data, payload);
+    ZBTxRequest zbTx = ZBTxRequest(addr64, send_data, sizeof(send_data));
+    xbee.send(zbTx);
 }
 
 void loop()
@@ -41,8 +78,7 @@ void loop()
 
         if (power_on != power_state) {
             power_state = power_on;
-            ZBTxRequest zbTx = ZBTxRequest(addr64, (uint8_t*)&power_on, sizeof(uint8_t));
-            xbee.send(zbTx);
+            send_to_xbee(power_on);
         }
     }
 }
