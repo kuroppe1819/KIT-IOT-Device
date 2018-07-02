0b117f3a38086b9aeb07b25705b71fae78567bc4
kuroppe <kuroppems@gmail.com>
2018-07-02 12:14:03 +0900

    Convert A.D. to 2 digit

--- RTC/RTC.ino
+++ RTC/RTC.ino
@@ -3,18 +3,22 @@
 
 RTC_DS1307 rtc;
 
-void setup() {
+void setup()
+{
     Serial.begin(9600);
     if (!rtc.begin()) {
         Serial.println("Couldn't find RTC");
-        while (1);
+        while (1)
+            ;
     }
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); //コンパイルした時の時間をRTCに記録する
 }
 
-void loop() {
+void loop()
+{
     DateTime now = rtc.now();
-    Serial.print(now.year());
+    uint8_t year = now.year() % 100; // 2018 -> 18に変換
+    Serial.print(year);
     Serial.print('/');
     Serial.print(now.month());
     Serial.print('/');
