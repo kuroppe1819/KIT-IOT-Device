0b60a8ed26fc4a4cf31e64dc8f7385946cded0ce
kuroppe <kuroppems@gmail.com>
2018-09-12 18:48:57 +0900

    Modified the communication format

--- KOBAYASHI_RX_BETA/KOBAYASHI_RX_BETA.ino
+++ KOBAYASHI_RX_BETA/KOBAYASHI_RX_BETA.ino
@@ -2,29 +2,17 @@
 #include <SoftwareSerial.h>
 #include <XBee.h>
 
+/*
 #define CURRENT_ID 0x00
 #define ENVIRONMENT_ID 0x01
 #define DUST_ID 0x02
-#define USE_NUMBER_OF_AREA 3
-
-const uint8_t send_frame_size = 13;
-const uint8_t send_data_size = 16;
-
+*/
 XBee xbee = XBee();
 XBeeResponse response = XBeeResponse();
 // create reusable response objects for responses we expect to handle
 ZBRxResponse rx = ZBRxResponse();
 
 SoftwareSerial monitor(8, 9); //(Rx, Tx)
-
-struct AreaData {
-    uint8_t current = 0xFE; //0xFEはそのセンサを使用していないことを示す
-    uint8_t temperature = 0xFE;
-    uint8_t humidity = 0xFE;
-    uint8_t illumination = 0xFE;
-    uint8_t dust = 0xFE;
-};
-struct AreaData area_data[USE_NUMBER_OF_AREA];
 
 void setup()
 {
@@ -35,48 +23,43 @@ void setup()
     monitor.println("Starting up!");
 }
 
-uint8_t calc_checksum(uint8_t* send_data)
+uint8_t calc_checksum(uint8_t* send_data, uint8_t frame_length)
 {
     uint16_t sum = 0;
-    for (int i = 0; i < send_frame_size; i++) {
-        sum += send_data[i + 2]; //2~14の範囲の合計
+    for (int i = 0; i < frame_length; i++) {
+        sum += send_data[i + 2];
     }
     return 0xFF - (sum & 0xFF); //チェックサムの計算
 }
 
-void serial_write(uint8_t* get_data, struct AreaData area_data)
+void serial_write(uint8_t* get_data, uint8_t get_data_length)
 {
-    uint8_t send_data[16] = {
-        0xFF, //スタートビット
-        send_frame_size,
-        get_data[1], //エリアコードの上位8bit
-        get_data[2], //エリアコードの下位8bit
-        get_data[3], //年
-        get_data[4], //月
-        get_data[5], //日
-        get_data[6], //時
-        get_data[7], //分
-        get_data[8], //秒
-        area_data.current,
-        area_data.temperature,
-        area_data.humidity,
-        area_data.illumination,
-        area_data.dust,
-        0x00 //チェックサム
-    };
+    uint8_t number_of_sensor = get_data_length - 9; //受信パケットからセンサデータ以外の要素を省く
+    uint8_t send_data_length = get_data_length + 4; //受信パケットサイズ + シリアル通信時に使用する符号の数
+    uint8_t send_data[send_data_length] = {};
 
-    send_data[send_data_size - 1] = calc_checksum(send_data);
-    monitor.write(send_data, send_data_size);
+    send_data[0] = 0xFF; //スタートビット
+    send_data[1] = get_data_length; //フレーム長(フレーム長とチェックサムに挟まれたバイト数)
+    for (int i = 0; i < get_data_length; i++) { //受信パケットを加工せずに追加する
+        send_data[i + 2] = get_data[i];
+    }
+    send_data[send_data_length - 2] = calc_checksum(send_data, get_data_length); //チェックサム: フレーム長 == 受信パケットサイズ
+    send_data[send_data_length - 1] = 0x00; //ストップビット
+    monitor.write(send_data, send_data_length);
 
-    // monitor.print("AreaCode=");
-    // monitor.println((rx.getData()[1] << 8) + rx.getData()[2]);
-    // for (int i = 0; i < send_data_size; i++) {
-    //     monitor.print("frame");
-    //     monitor.print("[");
-    //     monitor.print(i, DEC);
-    //     monitor.print("] is ");
-    //     monitor.println(send_data[i], HEX);
-    // }
+    print_send_data(send_data, send_data_length);
+}
+
+void print_send_data(uint8_t* send_data, uint8_t send_data_length)
+{
+    monitor.println("Display the send data");
+    for (int i = 0; i < send_data_length; i++) {
+        monitor.print("frame");
+        monitor.print("[");
+        monitor.print(i, DEC);
+        monitor.print("] is ");
+        monitor.println(send_data[i], HEX);
+    }
 }
 
 void loop()
@@ -85,26 +68,7 @@ void loop()
     if (xbee.getResponse().isAvailable()) {
         if (xbee.getResponse().getApiId() == ZB_RX_RESPONSE) {
             xbee.getResponse().getZBRxResponse(rx);
-            uint8_t h_digit = rx.getData()[1];
-            uint8_t l_digit = rx.getData()[2];
-            uint8_t index = (h_digit << 8) + l_digit;
-
-            switch (rx.getData()[0]) {
-            case CURRENT_ID:
-                area_data[index].current = rx.getData()[9];
-                break;
-            case ENVIRONMENT_ID:
-                area_data[index].temperature = rx.getData()[9];
-                area_data[index].humidity = rx.getData()[10];
-                area_data[index].illumination = rx.getData()[11];
-                break;
-            case DUST_ID:
-                area_data[index].dust = rx.getData()[9];
-                break;
-            default:
-                break;
-            }
-            serial_write(rx.getData(), area_data[index]);
+            serial_write(rx.getData(), rx.getDataLength());
         }
     } else if (xbee.getResponse().isError()) {
         monitor.print("error code:");
